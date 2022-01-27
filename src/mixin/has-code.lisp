991bac28bd7b2651d2ec4361a2144b187eb0a0b6
Tokuya Kameshima <kametoku@gmail.com>
2022-01-27 11:31:25 +0900

    mixin/has-code.lisp generate UUID for code by default.

--- src/mixin/has-code.lisp
+++ src/mixin/has-code.lisp
@@ -3,7 +3,7 @@
 (defclass has-code ()
   ((code :col-type (:varchar 64)
          :initarg :code
-         :initform (lack.util:generate-random-id)
+         :initform (uuid:make-v4-uuid)
          :accessor code))
   (:metaclass mito:dao-table-mixin)
   (:unique-keys code))
